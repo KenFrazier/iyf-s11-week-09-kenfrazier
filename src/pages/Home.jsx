799774e41c634a29f4a38523b41496b33d2f71e2
kenfrazier <njugunakamaufrazier13@gmail.com>
2026-08-12 11:44:49 +0300

    feat: add useLocalStorage, useToggle, useForm custom hooks; demo useToggle with Modal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,11 @@
+import Modal from '../components/Modal';
+
 function Home() {
   return (
     <div>
       <h2>Welcome to CommunityHub</h2>
       <p>This is the home page.</p>
+      <Modal />
     </div>
   );
 }
