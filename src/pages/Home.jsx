98b7313fe7a674a6a5c0da2b5eb167b2591bb345
kenfrazier <njugunakamaufrazier13@gmail.com>
2026-08-12 12:14:07 +0300

    feat: set up Tailwind CSS v4 with Vite plugin and content config

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import Modal from '../components/Modal';
 function Home() {
   return (
     <div>
-      <h2>Welcome to CommunityHub</h2>
+      <h2 className="text-3xl font-bold !text-purple-500">Welcome to CommunityHub</h2>
       <p>This is the home page.</p>
       <Modal />
     </div>
@@ -11,3 +11,4 @@ function Home() {
 }
 
 export default Home;
+
